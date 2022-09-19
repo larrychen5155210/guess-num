1c2c4cc79e591c6e2935930ef890d8b128d6b414
Larry Chen <z5155210@gmail.com>
2022-09-19 15:49:21 +0800

    update_1

--- 猜數字.py
+++ 猜數字.py
@@ -2,7 +2,9 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('請輸入數字: ')
 	num = int(num)
 	if num > r:
@@ -12,3 +14,5 @@ while True:
 	else:
 		print('你終於猜對了!!')
 		break
+	print('這是你猜的第', count, '次')
+print('你總共猜了', count, '次')
